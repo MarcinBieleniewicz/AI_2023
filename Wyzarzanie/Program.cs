55f392ae11d4958e14f8722a3484d978536de3fb
MarcinBieleniewicz <73874499+MarcinBieleniewicz@users.noreply.github.com>
2023-04-04 01:10:08 +0200

    Add files via upload

---
+++ Wyzarzanie/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Wyzarzanie;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SimulatedAnnealing sa = new SimulatedAnnealing();
+
+        Func<double, double> quadraticFunction = x => x * x - 4 * x + 3;
+
+        double xMin = -10;
+        double xMax = 10;
+        double initialTemperature = 1000;
+        double coolingRate = 0.99;
+        double minTemperature = 0.01;
+
+        double minX = sa.Minimize(quadraticFunction, xMin, xMax, initialTemperature, coolingRate, minTemperature);
+
+        Console.WriteLine($"Minimum found at x = {minX}, f(x) = {quadraticFunction(minX)}");
+    }
+}
---
+++ Wyzarzanie/SimulatedAnnealing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wyzarzanie
+{
+    internal class SimulatedAnnealing
+    {
+        private readonly Random _random = new Random();
+
+        public double Minimize(Func<double, double> function, double xMin, double xMax, double initialTemperature, double coolingRate, double minTemperature)
+        {
+            double currentX = _random.NextDouble() * (xMax - xMin) + xMin;
+            double bestX = currentX;
+            double currentEnergy = function(currentX);
+            double bestEnergy = currentEnergy;
+            double temperature = initialTemperature;
+
+            while (temperature > minTemperature)
+            {
+                double newX = _random.NextDouble() * (xMax - xMin) + xMin;
+                double newEnergy = function(newX);
+                double deltaEnergy = newEnergy - currentEnergy;
+
+                if (deltaEnergy < 0 || Math.Exp(-deltaEnergy / temperature) > _random.NextDouble())
+                {
+                    currentX = newX;
+                    currentEnergy = newEnergy;
+
+                    if (currentEnergy < bestEnergy)
+                    {
+                        bestX = currentX;
+                        bestEnergy = currentEnergy;
+                    }
+                }
+
+                temperature *= coolingRate;
+            }
+
+            return bestX;
+        }
+    }
+}
